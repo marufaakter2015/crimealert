aea3712810768b3d519a48ccea244bcf1baa1466
Marufa Akter <marufaakter2015@gmail.com>
2022-01-23 19:37:08 +0600

    welcome page connect

--- login.js
+++ login.js
@@ -46,7 +46,7 @@
   .then((userCredential) => {
     // Signed in
     var user = userCredential.user;
-
+    window.location = "http://127.0.0.1:5500/crimealert/welcome.html";
     // ...
   })
   .catch((error) => {
@@ -57,7 +57,7 @@
   });
   }
 
-function Redirect()
-{
-  window.location = "http://127.0.0.1:5500/crimealert/welcome.html";
-}
+// function Redirect()
+// {
+//   window.location = "http://127.0.0.1:5500/crimealert/welcome.html";
+// }
